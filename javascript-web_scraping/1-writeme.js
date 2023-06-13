faff81173b345153ed917d810714b6efd47b1644
Esther <e.mbanzabig@alustudent.com>
2023-06-13 12:20:25 +0200

    javascript

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,5 +1,19 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
-});
+
+function writeFile (filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(`An error occurred while writing to the file: ${err}`);
+    }
+  });
+}
+
+if (process.argv.length > 3) {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeFile(filePath, content);
+} else {
+  console.log('provide the file path and content as arguments.');
+}
